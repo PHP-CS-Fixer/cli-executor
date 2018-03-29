ef625639f1a655162ba6453023025bb48b20adc9
Dariusz Ruminski <dariusz.ruminski@gmail.com>
2018-03-29 17:48:15 +0200

    Introduce ScriptExecutor

--- src/BashScriptExecutor.php
+++ src/BashScriptExecutor.php
@@ -16,29 +16,9 @@ use Symfony\Component\Process\Process;
 final class BashScriptExecutor
 {
     /**
-     * @var int
+     * @var ScriptExecutor
      */
-    private static $tmpCounter = 0;
-
-    /**
-     * @var string[]
-     */
-    private $scriptParts;
-
-    /**
-     * @var string
-     */
-    private $cwd;
-
-    /**
-     * @var ?CliResult
-     */
-    private $result;
-
-    /**
-     * @var ?string
-     */
-    private $tmpFilePath;
+    private static $scriptExecutor;
 
     /**
      * @param string[] $scriptParts
@@ -46,15 +26,7 @@ final class BashScriptExecutor
      */
     public function __construct($scriptParts, $cwd)
     {
-        $this->scriptParts = $scriptParts;
-        $this->cwd = $cwd;
-    }
-
-    public function __destruct()
-    {
-        if (null !== $this->tmpFilePath) {
-            @unlink($this->tmpFilePath);
-        }
+        $this->scriptExecutor = new ScriptExecutor($scriptParts, $cwd, array('#!/usr/bin/env bash', 'set -e', ''));
     }
 
     /**
@@ -75,40 +47,6 @@ final class BashScriptExecutor
      */
     public function getResult()
     {
-        if (null === $this->result) {
-            $tmpFileName = 'tmp-'.self::$tmpCounter++.'.sh';
-            $tmpFileLines = array_merge(array('#!/usr/bin/env bash', 'set -e', ''), $this->scriptParts);
-            $this->tmpFilePath = $this->cwd.'/'.$tmpFileName;
-            file_put_contents($this->tmpFilePath, implode("\n", $tmpFileLines));
-            chmod($this->tmpFilePath, 0777);
-            $command = './'.$tmpFileName;
-
-            $process = new Process($command, $this->cwd);
-            $process->run();
-
-            $this->result = new CliResult(
-                $process->getExitCode(),
-                $process->getOutput(),
-                $process->getErrorOutput()
-            );
-        }
-
-        if (0 !== $this->result->getCode()) {
-            throw new ExecutionException(
-                $this->result,
-                sprintf(
-                    "Cannot execute `%s`:\n%s\nCode: %s\nExit text: %s\nError output: %s\nDetails:\n%s",
-                    $command,
-                    implode("\n", array_map(function ($line) { return "$ ${line}"; }, $tmpFileLines)),
-                    $this->result->getCode(),
-                    isset(Process::$exitCodes[$this->result->getCode()]) ? Process::$exitCodes[$this->result->getCode()] : 'Unknown exit code',
-                    $this->result->getError(),
-                    $this->result->getOutput()
-                ),
-                $process->getExitCode()
-            );
-        }
-
-        return $this->result;
+        return $this->scriptExecutor->getResult();
     }
 }
